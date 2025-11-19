425dfed248eabb10ed78cbe27dd94dfa0781685d
Cedric Moreau <cedric@moreau37.fr>
2025-11-19 21:33:23 +0100

    Few fixes

--- src/IbusCommands.js
+++ src/IbusCommands.js
@@ -1,83 +1,83 @@
 export const commands = {
-    DeviceStatusReq:             0x01,
-    DeviceStatus:                0x02,
-    BusStatusReq:                0x03,
-    BusStatus:                   0x04,
-    DiagMemoryRead:              0x06,
-    DiagMemoryWrite:             0x07,
-    DiagCodingRead:              0x08,
-    DiagCodingWrite:             0x09,
-    VehiculeCntrl:               0x0C,
-    IgnitionStatusReq:           0x10,
-    IgnitionStatus:              0x11,
-    SensorStatusRq:              0x12, /* IKE sensor status */
-    SensorStatus:                0x13, 
-    CountryCodingReq:            0x14,
-    CountryCoding:               0x15,
-    OdometerReq:                 0x16, /*"Odometer request"*/
-    Odometer:                    0x17, /*"Odometer"*/
-    SpeedRpm:                    0x18, /*"Speed/RPM"*/
-    Temperature:                 0x19, /*"Temperature"*/
-    TextGong:                    0x1A, /*"IKE text display/Gong"*/
-    TextStatus:                  0x1B, /*"IKE text status"*/
-    Gong:                        0x1C, /*"IKE Gong"*/
-    TemperatureReq:              0x1D,
-    TimeData:                    0x1F,
-    GTChangeUIReq:               0x20,
-    GTChangeUI:                  0x21,
-    GTMenuBuffer:                0x22, /* Display text ack */
-    GTWriteTitle:                0x23, /* Display text */
-    ANZVUpdate:                  0x24, /*"Update ANZV"*/
-    OBCSUpdate:                  0x2A, /*"On-Board Computer State Update"*/
-    TelephoneIndicator:          0x2B, /*Telephone indicators*/
-    TelephoneStatus:             0x2C,  /* telephone status */
-    GTMenuSelect:                0x31,
-    MFLButtons:                  0x32, /*"MFL buttons"*/
-    DSPEqButton:                 0x34, /*"DSP Equalizer Button"*/
-    GTDisplayRadioMenu:          0x37,
-    CDStatusReq:                 0x38, /*"CD status request"*/
-    CDStatus:                    0x39, /*"CD status"*/
-    MFLButtons2:                 0x3B, /*"MFL buttons"*/
-    SDRSStatusReq:               0x3D, /*"SDRS status request"*/
-    SDRSStatus:                  0x3E, /*"SDRS status"*/
-    OBCDSet:                     0x40, /*"Set On-Board Computer Data"*/
-    OBCDReq:                     0x41, /*"On-Board Computer Data Request"*/
-    GTScreenModeSet:             0x45,
-    LCDClear:                    0x46, /* LCD Clear*/
-    BMBTB0:                      0x47, /*"BMBT buttons"*/
-    BMBTB1:                      0x48, /*"BMBT buttons"*/
-    Knob:                        0x49, /*"KNOB button"*/ /*this is for right knob turn, pressing know is BMBTB1 and ButtonMenuKnob*/
-    K7Ctrl:                      0x4a, /*Cassette control*/
-    K7Status:                    0x4b, /*"Cassette Status"*/
-    GTRadioTelevisionStatus:     0x4E,
-    GTMonitorCtrl:               0x4F, /* RGB control */
-    VehiculeDataReq:             0x53, /*"Vehicle data request"*/
-    VehiculeData:                0x54, /*"Vehicle data status"*/
-    LampStatusReq:               0x5A, /*"Lamp status request"*/
-    LampStatus:                  0x5B, /*"Lamp Status"*/
-    InstrumentLightStatus:       0x5C, /*"Instrument cluster lighting status"*/
-    InstrumentLightStatusReq:	   0x5D, 
-    GTWriteIndex:                0x60,
-    GTWriteIndexTMC:             0x61,
-    GTWriteZone:                 0x62,
-    GTWriteStatic:               0x63,
-    RainStatusReq:               0x71, /*"Rain sensor status request"*/
-    RemoteKeyButton:             0x72, /*"Remote Key buttons"*/
-    EWSKeyStatus:                0x74, /*"EWS key status"*/
-    DoorsWindowsStatusReq:       0x79, /*"Doors/windows status request"*/
-    DoorsWindowsStatus:          0x7A, /*"Doors/windows status"*/
-    SHDStatus:                   0x7C, /*"SHD status"*/
-    DiagData:                    0xA0, /*"DIAG data"*/
-    GTTelematicsCoordinates:     0xA2,
-    GTTelematicsLocation:        0xA4,
-    GTWriteWithCursor:           0xA5, /* screen text */
-    SpecialIndicatorsAck:        0xA6, /* IKE special indicators ack */
-    TMCStatusReq:                0xA7, /*"TMC status request"*/
-    TMCStatus:                   0xA8,
-    NavigationTelephoneData:     0xA9,
-    NavigationCtrl:              0xAA, /*"Navigation Control"*/
-    NavigationRemoteCtrl:        0xAB, /* Remote control status */
-    RDSChannels:                 0xD4,  /*RDS channel list*/
+  DeviceStatusReq: 0x01,
+  DeviceStatus: 0x02,
+  BusStatusReq: 0x03,
+  BusStatus: 0x04,
+  DiagMemoryRead: 0x06,
+  DiagMemoryWrite: 0x07,
+  DiagCodingRead: 0x08,
+  DiagCodingWrite: 0x09,
+  VehiculeCntrl: 0x0c,
+  IgnitionStatusReq: 0x10,
+  IgnitionStatus: 0x11,
+  SensorStatusRq: 0x12 /* IKE sensor status */,
+  SensorStatus: 0x13,
+  CountryCodingReq: 0x14,
+  CountryCoding: 0x15,
+  OdometerReq: 0x16 /*"Odometer request"*/,
+  Odometer: 0x17 /*"Odometer"*/,
+  SpeedRpm: 0x18 /*"Speed/RPM"*/,
+  Temperature: 0x19 /*"Temperature"*/,
+  TextGong: 0x1a /*"IKE text display/Gong"*/,
+  TextStatus: 0x1b /*"IKE text status"*/,
+  Gong: 0x1c /*"IKE Gong"*/,
+  TemperatureReq: 0x1d,
+  TimeData: 0x1f,
+  GTChangeUIReq: 0x20,
+  GTChangeUI: 0x21,
+  GTMenuBuffer: 0x22 /* Display text ack */,
+  GTWriteTitle: 0x23 /* Display text */,
+  ANZVUpdate: 0x24 /*"Update ANZV"*/,
+  OBCSUpdate: 0x2a /*"On-Board Computer State Update"*/,
+  TelephoneIndicator: 0x2b /*Telephone indicators*/,
+  TelephoneStatus: 0x2c /* telephone status */,
+  GTMenuSelect: 0x31,
+  MFLButtons: 0x32 /*"MFL buttons"*/,
+  DSPEqButton: 0x34 /*"DSP Equalizer Button"*/,
+  GTDisplayRadioMenu: 0x37,
+  CDStatusReq: 0x38 /*"CD status request"*/,
+  CDStatus: 0x39 /*"CD status"*/,
+  MFLButtons2: 0x3b /*"MFL buttons"*/,
+  SDRSStatusReq: 0x3d /*"SDRS status request"*/,
+  SDRSStatus: 0x3e /*"SDRS status"*/,
+  OBCDSet: 0x40 /*"Set On-Board Computer Data"*/,
+  OBCDReq: 0x41 /*"On-Board Computer Data Request"*/,
+  GTScreenModeSet: 0x45,
+  LCDClear: 0x46 /* LCD Clear*/,
+  BMBTB0: 0x47 /*"BMBT buttons"*/,
+  BMBTB1: 0x48 /*"BMBT buttons"*/,
+  Knob: 0x49 /*"KNOB button"*/ /*this is for right knob turn, pressing know is BMBTB1 and ButtonMenuKnob*/,
+  K7Ctrl: 0x4a /*Cassette control*/,
+  K7Status: 0x4b /*"Cassette Status"*/,
+  GTRadioTelevisionStatus: 0x4e,
+  GTMonitorCtrl: 0x4f /* RGB control */,
+  VehiculeDataReq: 0x53 /*"Vehicle data request"*/,
+  VehiculeData: 0x54 /*"Vehicle data status"*/,
+  LampStatusReq: 0x5a /*"Lamp status request"*/,
+  LampStatus: 0x5b /*"Lamp Status"*/,
+  InstrumentLightStatus: 0x5c /*"Instrument cluster lighting status"*/,
+  InstrumentLightStatusReq: 0x5d,
+  GTWriteIndex: 0x60,
+  GTWriteIndexTMC: 0x61,
+  GTWriteZone: 0x62,
+  GTWriteStatic: 0x63,
+  RainStatusReq: 0x71 /*"Rain sensor status request"*/,
+  RemoteKeyButton: 0x72 /*"Remote Key buttons"*/,
+  EWSKeyStatus: 0x74 /*"EWS key status"*/,
+  DoorsWindowsStatusReq: 0x79 /*"Doors/windows status request"*/,
+  DoorsWindowsStatus: 0x7a /*"Doors/windows status"*/,
+  SHDStatus: 0x7c /*"SHD status"*/,
+  DiagData: 0xa0 /*"DIAG data"*/,
+  GTTelematicsCoordinates: 0xa2,
+  GTTelematicsLocation: 0xa4,
+  GTWriteWithCursor: 0xa5 /* screen text */,
+  SpecialIndicatorsAck: 0xa6 /* IKE special indicators ack */,
+  TMCStatusReq: 0xa7 /*"TMC status request"*/,
+  TMCStatus: 0xa8,
+  NavigationTelephoneData: 0xa9,
+  NavigationCtrl: 0xaa /*"Navigation Control"*/,
+  NavigationRemoteCtrl: 0xab /* Remote control status */,
+  RDSChannels: 0xd4 /*RDS channel list*/,
 };
 
 export function getCommandName(cmd) {
@@ -91,7 +91,6 @@ export function getCommandName(cmd) {
 }
 
 export default {
-    getCommandName,
-    commands
+  getCommandName,
+  commands,
 };
-
--- src/IbusInterface.js
+++ src/IbusInterface.js
@@ -1,143 +1,167 @@
-import { SerialPort } from 'serialport';
-import chalk from 'chalk';
-import { EventEmitter } from 'events';
-import IbusProtocol from './IbusProtocol.js';
-import IbusDevices from './IbusDevices.js';
-import debugLib from 'debug';
+import { SerialPort } from "serialport";
+import chalk from "chalk";
+import { EventEmitter } from "events";
+import IbusProtocol from "./IbusProtocol.js";
+import IbusDevices from "./IbusDevices.js";
+import debugLib from "debug";
 
-const debug = debugLib('IbusInterface');
+const debug = debugLib("IbusInterface");
 
 export default class IbusInterface extends EventEmitter {
-    constructor(devicePath = '/dev/ttyAMA0' ) {
-        super();
+  constructor(devicePath = "/dev/ttyAMA0") {
+    super();
 
-        this.device = devicePath;
-        this.serialPort = null;
-        this.parser = null;
+    this.device = devicePath;
+    this.serialPort = null;
+    this.parser = null;
+    this.lastActivityTime = process.hrtime();
+    this.queue = [];
+  }
+
+  setDevicePath(devicePath) {
+    this.device = devicePath;
+  }
+
+  initIBUS() {
+    this.serialPort = new SerialPort({
+      path: this.device,
+      baudRate: 9600,
+      parity: "none",
+      stopBits: 1,
+      dataBits: 8,
+      autoOpen: false,
+    });
+
+    this.serialPort.open((error) => {
+      debug("[IbusInterface] open");
+      if (error) {
+        console.log("[IbusInterface] Failed to open: " + error);
+        return;
+      }
+
+      console.log("[IbusInterface] Port Open [" + this.device + "]");
+
+      this.serialPort.on("data", (data) => {
         this.lastActivityTime = process.hrtime();
-        this.queue = [];
+      });
+
+      this.serialPort.on("error", (err) => {
+        console.log("[IbusInterface] Error", err);
+        this.shutdown(this.initIBUS.bind(this));
+      });
+
+      this.parser = new IbusProtocol();
+      this.parser.on("message", this.onMessage.bind(this));
+
+      this.serialPort.pipe(this.parser);
+
+      this.watchForEmptyBus(this.processWriteQueue.bind(this));
+    });
+  }
+
+  getHrDiffTime(time) {
+    const ts = process.hrtime(time);
+    return ts[0] * 1000 + ts[1] / 1000000;
+  }
+
+  watchForEmptyBus(workerFn) {
+    if (this.getHrDiffTime(this.lastActivityTime) >= 20) {
+      workerFn(() => {
+        setImmediate(this.watchForEmptyBus.bind(this), workerFn);
+      });
+    } else {
+      setImmediate(this.watchForEmptyBus.bind(this), workerFn);
+    }
+  }
+
+  processWriteQueue(ready) {
+    if (this.queue.length <= 0) {
+      ready();
+      return;
     }
 
-    setDevicePath(devicePath) {
-        this.device = devicePath;
-    }
+    const dataBuf = this.queue.pop();
+    debug(
+      chalk.blue("[IbusInterface] Write queue length: "),
+      this.queue.length
+    );
 
-    initIBUS() {
-        this.serialPort = new SerialPort({
-            path: this.device,
-            baudRate: 9600,
-            parity: 'none',
-            stopBits: 1,
-            dataBits: 8,
-            autoOpen: false
+    this.serialPort.write(dataBuf, (error, resp) => {
+      if (error) {
+        console.log("[IbusInterface] Failed to write: " + error);
+      } else {
+        debug(
+          "[IbusInterface] ",
+          chalk.white("Wrote to Device: "),
+          dataBuf,
+          resp
+        );
+
+        this.serialPort.drain((error) => {
+          debug(chalk.white("Data drained"));
+          this.lastActivityTime = process.hrtime();
+          ready();
         });
+      }
+    });
+  }
 
-        this.serialPort.open((error) => {
-            debug('[IbusInterface] open');
-            if (error) {
-                console.log('[IbusInterface] Failed to open: ' + error);
-                return;
-            }
+  closeIBUS(callback) {
+    this.serialPort.close((error) => {
+      if (error) {
+        console.log("[IbusInterface] Error closing port: ", error);
+      } else {
+        debug("[IbusInterface] Port Closed [" + this.device + "]");
+        this.parser = null;
+      }
+      callback();
+    });
+  }
 
-            console.log('[IbusInterface] Port Open [' + this.device + ']');
+  getInterface() {
+    return this.serialPort;
+  }
 
-            this.serialPort.on('data', (data) => {
-                this.lastActivityTime = process.hrtime();
-            });
+  startup() {
+    this.initIBUS();
+  }
 
-            this.serialPort.on('error', (err) => {
-                console.log("[IbusInterface] Error", err);
-                this.shutdown(this.initIBUS.bind(this));
-            });
+  shutdown(callback) {
+    debug("[IbusInterface] Shutting down Ibus device..");
+    this.closeIBUS(callback);
+  }
 
-            this.parser = new IbusProtocol();
-            this.parser.on('message', this.onMessage.bind(this));
-
-            this.serialPort.pipe(this.parser);
-
-            this.watchForEmptyBus(this.processWriteQueue.bind(this));
-        });
+  onMessage(msg) {
+    // correct msg
+    if (!msg || !msg.msg || msg.msg.length === 0) {
+      console.warn("[IbusInterface] Ignoring invalid message:", msg);
+      return;
     }
 
-    getHrDiffTime(time) {
-        const ts = process.hrtime(time);
-        return (ts[0] * 1000) + (ts[1] / 1000000);
+    debug(
+      "[IbusInterface] Raw Message: ",
+      msg.src,
+      msg.len,
+      msg.dst,
+      msg.msg,
+      "[" + msg.msg.toString("ascii") + "]",
+      msg.crc
+    );
+    this.emit("data", msg);
+  }
+
+  sendMessage(msg) {
+    const dataBuf = IbusProtocol.createIbusMessage(msg);
+    debug("[IbusInterface] Send message: ", dataBuf);
+
+    if (this.queue.length > 1000) {
+      console.log(
+        "[IbusInterface] Queue too large, dropping message..",
+        dataBuf
+      );
+      return;
     }
 
-    watchForEmptyBus(workerFn) {
-        if (this.getHrDiffTime(this.lastActivityTime) >= 20) {
-            workerFn(() => {
-                setImmediate(this.watchForEmptyBus.bind(this), workerFn);
-            });
-        } else {
-            setImmediate(this.watchForEmptyBus.bind(this), workerFn);
-        }
-    }
-
-    processWriteQueue(ready) {
-        if (this.queue.length <= 0) {
-            ready();
-            return;
-        }
-
-        const dataBuf = this.queue.pop();
-        debug(chalk.blue('[IbusInterface] Write queue length: '), this.queue.length);
-
-        this.serialPort.write(dataBuf, (error, resp) => {
-            if (error) {
-                console.log('[IbusInterface] Failed to write: ' + error);
-            } else {
-                debug('[IbusInterface] ', chalk.white('Wrote to Device: '), dataBuf, resp);
-
-                this.serialPort.drain((error) => {
-                    debug(chalk.white('Data drained'));
-                    this.lastActivityTime = process.hrtime();
-                    ready();
-                });
-            }
-        });
-    }
-
-    closeIBUS(callback) {
-        this.serialPort.close((error) => {
-            if (error) {
-                console.log('[IbusInterface] Error closing port: ', error);
-            } else {
-                debug('[IbusInterface] Port Closed [' + this.device + ']');
-                this.parser = null;
-            }
-            callback();
-        });
-    }
-
-    getInterface() {
-        return this.serialPort;
-    }
-
-    startup() {
-        this.initIBUS();
-    }
-
-    shutdown(callback) {
-        debug('[IbusInterface] Shutting down Ibus device..');
-        this.closeIBUS(callback);
-    }
-
-    onMessage(msg) {
-        debug('[IbusInterface] Raw Message: ', msg.src, msg.len, msg.dst, msg.msg, '[' + msg.msg.toString('ascii') + ']', msg.crc);
-        this.emit('data', msg);
-    }
-
-    sendMessage(msg) {
-        const dataBuf = IbusProtocol.createIbusMessage(msg);
-        debug('[IbusInterface] Send message: ', dataBuf);
-
-        if (this.queue.length > 1000) {
-            console.log('[IbusInterface] Queue too large, dropping message..', dataBuf);
-            return;
-        }
-
-        this.queue.unshift(dataBuf);
-    }
+    this.queue.unshift(dataBuf);
+  }
 }
-
